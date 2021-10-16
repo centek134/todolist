1142d1c3ea5167c59459392e99a892b2f3534cee
centek134 <centek134@wp.pl>
2021-10-16 09:36:52 +0200

    We can can delete completed tasks

--- script/app.ts
+++ script/app.ts
@@ -6,10 +6,8 @@ const tasks: {title: string, description: string, id:number}[] = [];
 
 
 const getInputValues = () => {
-    console.log(title.value);
     pushTask(title.value,description.value);
 };
-
 const pushTask = (title:string ,desc:string) => {
     tasks.push(
         {title: title,
@@ -18,24 +16,35 @@ const pushTask = (title:string ,desc:string) => {
     );
     renderTask();
 };
-
 const renderTask = () => {
-        const divEl =  document.createElement("div");
+        const divEl =  document.createElement("div") as HTMLDivElement;
         divEl.classList.add("task_card");
         divEl.appendChild(createHeaderNode(tasks[tasks.length-1].title));
         divEl.appendChild(createDescriptionNode(tasks[tasks.length-1].description));
+        divEl.appendChild(createButton(tasks[tasks.length-1].id,divEl));
         tasksSection.appendChild(divEl);
 };
 const createHeaderNode = (text:string) => {
-    const hElement = document.createElement("h2");
+    const hElement = document.createElement("h2") as HTMLHeadingElement;
     hElement.innerHTML = text;
     return hElement;
 };
 const createDescriptionNode = (text:string) => {
-    const pElement = document.createElement("p");
+    const pElement = document.createElement("p") as HTMLParagraphElement;
     pElement.innerHTML = text;
     return pElement;
 };
+const createButton = (id:number,child: HTMLDivElement) => {
+    const buttonElement = document.createElement("button") as HTMLButtonElement;
+    buttonElement.innerHTML = "Complete";
+    buttonElement.classList.add("task_card_button");
+    buttonElement.addEventListener("click", () => {
+            tasks.splice(tasks.findIndex(element => element.id === id ,1));
+            tasksSection.removeChild(child)
+    });
+    return buttonElement;
+};
+
 
 
 addButton.addEventListener("click", getInputValues);
