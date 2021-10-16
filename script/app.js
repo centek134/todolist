6dfba1279f6417efc99d7ea3f7a320f1cb9a6ec5
centek134 <centek134@wp.pl>
2021-10-16 09:37:32 +0200

    we can now delete completed tasks

--- script/app.js
+++ script/app.js
@@ -5,7 +5,6 @@ const addButton = document.getElementById("add-button");
 const tasksSection = document.getElementById("tasks");
 const tasks = [];
 const getInputValues = () => {
-    console.log(title.value);
     pushTask(title.value, description.value);
 };
 const pushTask = (title, desc) => {
@@ -19,6 +18,7 @@ const renderTask = () => {
     divEl.classList.add("task_card");
     divEl.appendChild(createHeaderNode(tasks[tasks.length - 1].title));
     divEl.appendChild(createDescriptionNode(tasks[tasks.length - 1].description));
+    divEl.appendChild(createButton(tasks[tasks.length - 1].id, divEl));
     tasksSection.appendChild(divEl);
 };
 const createHeaderNode = (text) => {
@@ -30,5 +30,15 @@ const createDescriptionNode = (text) => {
     const pElement = document.createElement("p");
     pElement.innerHTML = text;
     return pElement;
+};
+const createButton = (id, child) => {
+    const buttonElement = document.createElement("button");
+    buttonElement.innerHTML = "Complete";
+    buttonElement.classList.add("task_card_button");
+    buttonElement.addEventListener("click", () => {
+        tasks.splice(tasks.findIndex(element => element.id === id, 1));
+        tasksSection.removeChild(child);
+    });
+    return buttonElement;
 };
 addButton.addEventListener("click", getInputValues);
